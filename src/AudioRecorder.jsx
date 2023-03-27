056f814265e1dfb5fc17506661d2b9da10ed2a7c
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-03-27 16:07:04 +0530

    updating

--- src/AudioRecorder.jsx
+++ src/AudioRecorder.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useRef } from "react";
 import { Configuration } from "openai";
 import { OpenAIApi } from "openai";
+import { logDOM } from "@testing-library/react";
 const configuration = new Configuration({
-      apiKey: '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'
-    });
+  apiKey: "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",
+});
 const AudioRecorder = () => {
   const [recording, setRecording] = useState(false);
   const [audioChunks, setAudioChunks] = useState([]);
@@ -43,68 +44,64 @@ const AudioRecorder = () => {
     link.click();
   };
 
-
-
   const handleUpload = async () => {
     const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+    const model = "whisper-1";
     console.log(audioBlob);
-    // const formData = new FormData();
-    // formData.append("file", "audioBlob");
-    // formData.append("model", "whisper-1");
 
-    const formData = {
-      file:audioBlob,
-      model:"whisper-1"
-    }
+    const formData = new FormData();
+    formData.append("file", audioBlob);
+    formData.append("model", model);
 
+    // const formData = {
+    //   file:audioBlob,
+    //   model:"whisper-1"
+    // }
 
     fetch("https://api.openai.com/v1/audio/transcriptions", {
       method: "POST",
       headers: {
         "Content-Type": "audio/webm",
         Authorization: `Bearer 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`,
-        
+
       },
-      body:JSON.stringify(formData),
+      body:formData,
     })
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
+      console.log(formData);
 
-    
     // const openai = new OpenAIApi(configuration);
-    // const resp = await openai.createTranscription({
-    //   file:audioBlob,
-    //   model:"whisper-1"
-    //  });
+    // const resp = await openai.createTranscription({formData});
     // console.log(resp);
   };
-
+  const style = "border-[1px] border-black m-1 p-1 rounded-md";
   return (
     <div>
       <button
-        className="border-[1px] border-black"
+        className={style}
         onClick={handleStartRecording}
         disabled={recording}
       >
         {recording ? "Recording..." : "Record"}
       </button>
       <button
-        className="border-[1px] border-black"
+        className={style}
         onClick={handleStopRecording}
         disabled={!recording}
       >
         Stop
       </button>
       <button
-        className="border-[1px] border-black"
+        className={style}
         onClick={handleDownload}
         disabled={!audioChunks.length}
       >
         Download
       </button>
       <button
-        className="border-[1px] border-black"
+        className={style}
         onClick={handleUpload}
         disabled={!audioChunks.length}
       >
